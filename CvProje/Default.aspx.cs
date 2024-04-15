3ef1e6e22a2e96107558a8a0ce6bdc70dddb0ce8
Rita <Rita@DESKTOP-UEP6LN2>
2024-04-15 11:50:58 +0300

    Add project files.

---
+++ CvProje/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CvProje
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        DBCVENTITYEntities db = new DBCVENTITYEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Repeater1.DataSource = db.TBL_HAKKIMDA.ToList();
+            Repeater1.DataBind();
+
+            Repeater2.DataSource = db.TBL_HAKKIMDA.ToList();
+            Repeater2.DataBind();
+
+            Repeater3.DataSource = db.TBL_HAKKIMDA.ToList();
+            Repeater3.DataBind();
+        }
+    }
+}
